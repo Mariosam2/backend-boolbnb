e6698e2f9c62b77a96c48372703352c8f444897f
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-03 15:47:44 +0100

    adding test file

---
+++ config/apartments.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'apartments' => [
+        [
+            "user_id" => 1,
+            "apartment_category_id" => 1,
+            "address" => "Ljutomer, Slovenia",
+            "check_in" => "14:00-22:00",
+            "check_out" => "00:00-10:00",
+            "price" => 73,
+            "media" => "https://a0.muscache.com/im/pictures/d7c1f140-c33a-4d68-aaf8-b7b8d7292b11.jpg",
+            "beds" => 2,
+            "title" => "Appartamento Zemlyanka - Casa di terra",
+            "description" => "Con un ritmo di vita faticoso, preoccupazioni costanti, oneri e obblighi sempre nuovi, c'è sempre bisogno di trovare il tempo per IL RIPOSO e IL RELAX.
+            Fuori dal trambusto della città, nel bel mezzo di un bellissimo PAESAGGIO a Razkrižje, c'è una MODERNA CASA IN TERRA / HOBIT, che impressiona ogni ospite. È interamente costruita con materiali naturali (ARGILLA, LEGNO,...). L'amore per la creatività e la natura si sente ad ogni passo. Le testimonianze sono capolavori artigianali nella terra e intorno ad essa.",
+            "guests" => 4,
+            "baths" => 1,
+            "visible" => true,
+            "mq" => 70,
+            "latitude" => "46.516266",
+            "longitude" => "16.196036",
+        ],
+    ]
+];
