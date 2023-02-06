22ba07b57d5ad43993291292ef3d993d1a9b336c
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-06 13:49:16 +0100

    adding sidebar layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,44 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Work+Sans:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
 
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <div id="app">
 
-        @include('partials.header')
-        {{-- @include('partials.leftmenu') --}}
 
 
         <main class="">
-            @yield('content')
-        </main>
-    </div>
+            <div class="container-fluid">
+                <div class="row">
+                    @include('partials.sidebar')
+
+
+
+                    @yield('content')
+
+                </div>
 </body>
 
 </html>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,53 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse min-vh-100 ms_sidebar ps-5 py-5">
+    <div class="position-sticky pt-3 sidebar-sticky h-100 d-flex flex-column">
+        <h1 class="text-uppercase text_lighter font_secondary mb-5">
+            B<svg class="infinite_logo" version="1.0" xmlns="http://www.w3.org/2000/svg" width="64.000000pt"
+                height="64.000000pt" viewBox="0 0 64.000000 64.000000" preserveAspectRatio="xMidYMid meet">
+
+                <g transform="translate(0.000000,64.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+                    <path
+                        d="M85 440 c-23 -11 -48 -33 -58 -49 -22 -37 -22 -105 0 -142 18 -30 84
+           -69 118 -69 30 0 98 38 118 66 26 35 13 43 -21 12 -56 -53 -105 -61 -158 -29
+           -90 55 -47 201 59 201 46 0 79 -24 170 -120 84 -89 141 -130 181 -130 11 0 39
+           9 62 20 99 48 99 192 0 240 -23 11 -51 20 -61 20 -30 0 -98 -38 -118 -66 -26
+           -35 -13 -43 21 -12 56 53 105 61 158 29 66 -40 66 -142 0 -182 -69 -42 -112
+           -23 -229 101 -84 90 -141 130 -182 130 -11 0 -38 -9 -60 -20z" />
+                </g>
+            </svg>lBnB
+        </h1>
+        <ul class="nav flex-column mt-5">
+            <li class="nav-item">
+                <a href="{{ route('dashboard') }}" class="nav-link active">
+                    <span class="align-text-bottom"></span>
+                    <i class="fa-solid fa-chart-line fa-fw"></i>Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('apartments.index') }}" class="nav-link active">
+                    <span class="align-text-bottom"></span>
+                    <i class="fa-solid fa-house fa-fw"></i></i>Appartamenti
+                </a>
+            </li>
+        </ul>
+        <ul class="nav flex-column mt-auto">
+            <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <span class="align-text-bottom"></span>
+                    <i class="fa-solid fa-gear fa-fw"></i></i>Settings
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                    class="nav-link active">
+                    <span class="align-text-bottom"></span>
+                    <i class="fa-solid fa-arrow-right-from-bracket fa-fw"></i></i>Sign out
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+
+    </div>
+</nav>
