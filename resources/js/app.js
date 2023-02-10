697597e0e66a18d2d3cab68d445331a15a8fd7a7
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-10 14:48:58 +0100

    fixing autocomplete input address

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
-import './tomtom'
 import.meta.glob([
     '../img/**'
 ])
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -19,6 +19,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!--TomTom-->
+    <link rel="stylesheet" type="text/css"
+        href="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox.css" />
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/plugins/SearchBox/3.1.3-public-preview.0/SearchBox-web.js">
+    </script>
+    <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.1.2-public-preview.15/services/services-web.min.js">
+    </script>
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
 </head>
@@ -36,7 +43,52 @@
 
 
                     @yield('content')
+                    <script type="text/javascript">
+                        const searchBoxWrapper = document.querySelector('.searchBoxWrapper');
+                        const options = {
+                            searchOptions: {
+                                key: "Ad83Ah6WsxYFXscdqk3lFXmhKanlaKHs",
+                                language: "it-IT",
+                                limit: 5,
+                            },
+                            autocompleteOptions: {
+                                key: "Ad83Ah6WsxYFXscdqk3lFXmhKanlaKHs",
+                                language: "it-IT",
+                            },
+
+                        }
+                        const ttSearchBox = new tt.plugins.SearchBox(tt.services, options)
+                        const searchBoxHTML = ttSearchBox.getSearchBoxHTML()
+                        searchBoxWrapper.append(searchBoxHTML)
+                        const searchBoxInput = document.querySelector('.tt-search-box-input');
+                        const resultsContainer = document.querySelector('.tt-search-box-result-list-container')
+                        searchBoxInput.name = 'address';
+                        searchBoxInput.id = 'address'
+                        searchBoxInput.setAttribute('value',
+                            '{{ isset($apartment) ? old('address', $apartment->address) : old('address') }}')
+
+                        searchBoxInput.addEventListener('input', function() {
+                            if (this.value == '') {
+                                resultsContainer.style.display = "none";
+                            } else {
+                                resultsContainer.style.display = "block";
+                            }
+                        })
+                        searchBoxInput.addEventListener('blur', function() {
+                            if (this.value == '') {
+                                this.value = '{{ isset($apartment) ? old('address', $apartment->address) : old('address') }}'
+                            }
+                        })
+                        searchBoxInput.addEventListener('focus', function() {
+                            if (this.value == '') {
+                                this.value = '{{ isset($apartment) ? old('address', $apartment->address) : old('address') }}'
+                            }
+                        })
+                    </script>
                 </div>
+            </div>
+        </main>
+
 </body>
 
 </html>
