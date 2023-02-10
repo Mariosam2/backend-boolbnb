61093e6b1db0ea49726bf619ef6ee3e2ed11e451
karfaisarra <karfaisarra@gmail.com>
2023-02-10 10:18:44 +0100

    fix show

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -10,15 +10,16 @@
             <p>{{ $apartment->description }}</p>
             <ul class="list-unstyled">
                 <li><strong>Indirizzo: </strong>{{ $apartment->address }}</li>
+                <li><strong>Categoria: </strong>{{ $apartment->apartment_category->name }}</li>
                 <li><strong>Metri quadrati: </strong>{{ $apartment->mq }}m²</li>
                 <li><strong>Camere: </strong>{{ $apartment->total_rooms }}</li>
                 <li><strong>Letti: </strong>{{ $apartment->beds }}</li>
                 <li><strong>Bagni: </strong> {{ $apartment->baths }}</li>
                 <li><strong>Ospiti: </strong> {{ $apartment->guests }}</li>
                 <li><strong>Prezzo: </strong> {{ $apartment->price }}$</li>
-                <li><strong>Check_in: </strong> {{ $apartment->Check_in }} </li>
-                <li><strong>Check_out: </strong> {{ $apartment->Check_out }} </li>
-                <li><strong>Services:</strong> <br>
+                <li><strong>Check_in: </strong> {{ $apartment->check_in }} </li>
+                <li><strong>Check_out: </strong> {{ $apartment->check_out }} </li>
+                <li><strong>Servizi:</strong> <br>
                     @if (count($apartment->services) > 0)
                         @foreach ($apartment->services as $service)
                 <li>{{ $service->name }}</li>
