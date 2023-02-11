df1b9bcb726c6666f234072dc196c8562bde86bf
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-11 21:21:53 +0100

    adding search api route

--- app/Http/Controllers/API/ApartmentController.php
+++ app/Http/Controllers/API/ApartmentController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchApartmentRequest;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ApartmentController extends Controller
 {
@@ -18,7 +20,7 @@ class ApartmentController extends Controller
     public function show($slug)
     {
         $apartment = Apartment::with('user', 'services', 'views', 'promotions', 'apartment_category')->where('slug', $slug)->first();
-        
+
         if ($apartment) {
             return response()->json([
                 'success' => true,
@@ -27,8 +29,39 @@ class ApartmentController extends Controller
         } else {
             return response()->json([
                 'success' => false,
-                'results' => 'apartment Not Found'
+                'results' => 'apartment not found'
             ]);
+        }
+    }
+
+    public function search(Request $request)
+    {
+        //dd($request->services);
+        $data = [
+            'address' => $request->address,
+            'services' => $request->services
+        ];
+
+        //dd($data);
+
+        $validator = Validator::make($data, [
+            'address' => 'required|max:255',
+            'services' => 'nullable|array|exists:services,id'
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'results' => 'invalid request'
+            ]);
+        } else {
+            //filter data
+            $val_data = $validator->validate();
+            dd($val_data);
+            if (isset($val_data['services'])) {
+            } else {
+            }
         }
     }
 }
