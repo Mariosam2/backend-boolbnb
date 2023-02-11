26cdb8ceb2c1db6d3de894a887d0c23523e3c0c9
karfaisarra <karfaisarra@gmail.com>
2023-02-11 17:29:54 +0100

    annd controller and route api post

---
+++ app/Http/Controllers/API/MessageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use App\Mail\NewMessage;
+use App\Models\Message;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'apartment_id' => 'required|numeric',
+            'name' => 'required',
+            'surname' => 'required',
+            'email' => 'required|email',
+            'body' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+        $newMessage = new Message();
+        $newMessage->fill($data);
+        $newMessage->save();
+        
+        Mail::to('exemple@gmail.com')->send(new NewMessage($newMessage));
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
