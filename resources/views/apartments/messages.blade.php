838309fd0ccf90087631fbb739e5d7e24a3cd076
karfaisarra <karfaisarra@gmail.com>
2023-02-13 22:17:31 +0100

    add condition

--- resources/views/apartments/messages.blade.php
+++ resources/views/apartments/messages.blade.php
@@ -4,29 +4,33 @@
         <h1 class="my-5">Tutti i messaggi</h1>
 
         @foreach ($apartments as $apartment)
-            @foreach ($apartment->messages as $message)
-                <div class="col">
-                    <div class="card mb-5 w-25">
-                        <div class="details p-3">
-                            <div>
-                                <strong>Nome appartamento:</strong> {{ $apartment->title }}
-                            </div>
-                            <div>
-                                <strong>Nome:</strong> {{ $message->name }}
-                            </div>
-                            <div>
-                                <strong>Cognome:</strong> {{ $message->surname }}
-                            </div>
-                            <div>
-                                <strong>Email:</strong> {{ $message->email }}
-                            </div>
-                            <div>
-                                <strong>Messagio:</strong> {{ $message->body }}
+            @if ($apartment->messages)
+                @foreach ($apartment->messages as $message)
+                    <div class="col">
+                        <div class="card mb-5 w-25">
+                            <div class="details p-3">
+                                <div>
+                                    <strong>Nome appartamento:</strong> {{ $apartment->title }}
+                                </div>
+                                <div>
+                                    <strong>Nome:</strong> {{ $message->name }}
+                                </div>
+                                <div>
+                                    <strong>Cognome:</strong> {{ $message->surname }}
+                                </div>
+                                <div>
+                                    <strong>Email:</strong> {{ $message->email }}
+                                </div>
+                                <div>
+                                    <strong>Messagio:</strong> {{ $message->body }}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @else
+                <p>Non ci sono messaggi!</p>
+            @endif
         @endforeach
     </div>
 @endsection
