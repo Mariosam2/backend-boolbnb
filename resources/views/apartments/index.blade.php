85b73ffcf6b073ddd4cfefea5948f1aa62bab46e
karfaisarra <karfaisarra@gmail.com>
2023-02-14 13:28:43 +0100

    add style to index

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.user')
 @section('content')
     <div class="col d-flex flex-column justify-content-center">
-        <div class="container apartments ms-xxl-2 p-xxl-5">
+        <div class="apartments py-5">
             <h1>Appartamenti</h1>
-            <a name="" id="" class="btn btn-primary" href="{{ route('apartments.create') }}"
-                role="button">Aggiungi un appartamento
+            <a name="" id="" class="btn create_btn text-white mb-5" href="{{ route('apartments.create') }}"
+                role="button">Aggiungi
+                un appartamento
                 <i class="fa fa-plus-circle" aria-hidden="true"></i>
             </a>
             @if (session('message'))
@@ -12,62 +13,66 @@
                     {{ session('message') }}
                 </div>
             @endif
-            <div class="row row-cols-1">
+            <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
                 @forelse($apartments as $apartment)
                     <div class="col">
-                        <div class="card ms_card d-flex flex-row flex-wrap flex-xxl-nowrap my-3">
-                            <img class="apartment-img"
+                        <div class="card my_card my-3 rounded-0 border-0">
+                            <img class="my-img img-fluid"
                                 src="{{ Storage::exists($apartment->media) ? asset('storage/' . $apartment->media) : $apartment->media }}"
                                 alt="Title">
-                            <div class="card-body">
-                                <h4 class="card-title">{{ $apartment->title }}</h4>
-                                <p class="card-text">{{ $apartment->description }}</p>
-                                <a href="http://localhost:5174/blog/{{ $apartment->slug }}">
-                                    <i class="fas fa-eye fa-sm fa-fw"></i>
-                                </a>
-                                <!-- edit -->
-                                <a href="{{ route('apartments.edit', $apartment->slug) }}">
-                                    <i class="fas fa-pencil fa-sm fa-fw"></i>
-                                </a>
+                            <div class="card-body ">
+                                <h4 class="">{{ $apartment->title }}</h4>
+                                <div class="text-end">
+                                    <a href="http://localhost:5174/blog/{{ $apartment->slug }}"
+                                        class="btn show_btn text-white">
+                                        <i class="fas fa-eye fa-sm fa-fw"></i>
+                                    </a>
+                                    <!-- edit -->
+                                    <a href="{{ route('apartments.edit', $apartment->slug) }}"
+                                        class="btn edit_btn text-white">
+                                        <i class="fas fa-pencil fa-sm fa-fw"></i>
+                                    </a>
 
-                                <!-- Modal trigger button -->
-                                <a type="button" data-bs-toggle="modal" data-bs-target="#apartment-{{ $apartment->slug }}"
-                                    style="color: #0d6efd">
-                                    <i class="fa-solid fa-trash"></i>
+                                    <!-- Modal trigger button -->
+                                    <a type="button" data-bs-toggle="modal"
+                                        data-bs-target="#apartment-{{ $apartment->slug }}"
+                                        class="btn delete_btn text-white">
+                                        <i class="fa-solid fa-trash"></i>
 
-                                </a>
+                                    </a>
 
-                                <!-- Modal Body -->
-                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                <div class="modal fade" id="apartment-{{ $apartment->slug }}" tabindex="-1"
-                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                    aria-labelledby="modal-{{ $apartment->slug }}" aria-hidden="true">
-                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                                        role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="modal-{{ $apartment->slug }}">Eliminare
-                                                    {{ $apartment->title }}</h5>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                    aria-label="Close">
+                                    <!-- Modal Body -->
+                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                    <div class="modal fade" id="apartment-{{ $apartment->slug }}" tabindex="-1"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                        aria-labelledby="modal-{{ $apartment->slug }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                            role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="modal-{{ $apartment->slug }}">Eliminare
+                                                        {{ $apartment->title }}</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close">
 
 
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Sei sicuro di voler eliminare questo appartamento?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary"
-                                                    data-bs-dismiss="modal">Chiudi</button>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Sei sicuro di voler eliminare questo appartamento?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Chiudi</button>
 
-                                                <form action="{{ route('apartments.destroy', $apartment->slug) }}"
-                                                    method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger">Elimina</button>
-                                                </form>
+                                                    <form action="{{ route('apartments.destroy', $apartment->slug) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger">Elimina</button>
+                                                    </form>
 
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
