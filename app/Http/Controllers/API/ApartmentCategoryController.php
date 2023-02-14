eb2f06501412a12b42afb1b1b4390b9e3d45959b
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-14 16:54:06 +0100

    adding img to apartment categories

--- app/Http/Controllers/API/ApartmentCategoryController.php
+++ app/Http/Controllers/API/ApartmentCategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\ApartmentCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ApartmentCategoryController extends Controller
 {
@@ -12,6 +13,11 @@ class ApartmentCategoryController extends Controller
     {
         $apartment_categories = ApartmentCategory::all();
         if (count($apartment_categories) > 0) {
+            foreach ($apartment_categories as $apartment_category) {
+                $apartment_category->name = str_replace('\'', ' ', $apartment_category->name);
+                $apartment_category->img =  Str::slug($apartment_category->name . ' ' . $apartment_category->id);
+            }
+            //dd($apartment_categories);
             return response()->json([
                 "success" => true,
                 "results" => $apartment_categories
