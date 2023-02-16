ce6665a46ceb64a9f04afe33e3c59911ba1f2e7d
MicheleRossetti02 <michelerossetti07@gmail.com>
2023-02-16 16:16:25 +0100

    to fix

--- resources/views/promotions/plans.blade.php
+++ resources/views/promotions/plans.blade.php
@@ -3,12 +3,27 @@
 
 
 @section('content')
-    <div class="col">
+    @forelse($promotions as $promotion)
+        <div class="col-1 col-md-3 text-center  ">
 
-        @forelse($promotions as $promotion)
-            <a class="d-block pt-5 mx-4"
-                href="{{ route('promo.purchase', ['apartment' => $apartment->slug, 'promotion' => $promotion->id]) }}">{{ $promotion->name }}</a>
-        @empty
-        @endforelse
-    </div>
+            <div class="card mb-4 rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <a class="d-block pt-5 mx-4 text-uppercase"
+                        href="{{ route('promo.purchase', ['apartment' => $apartment->slug, 'promotion' => $promotion->id]) }}">{{ $promotion->name }}</a>
+
+                </div>
+
+            </div>
+
+            <div class="card-body">
+                <h1>
+
+                </h1>
+
+                <button type="button" class="w-100 btn btn-lg btn-outline-primary">Subscripe</button>
+            </div>
+        </div>
+
+    @empty
+    @endforelse
 @endsection
