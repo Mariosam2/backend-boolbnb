bcd7fd4004162e53a23fbb95351dcb50eab34812
karfaisarra <karfaisarra@gmail.com>
2023-02-16 15:15:15 +0100

    add Responsive layout

--- resources/views/apartments/messages.blade.php
+++ resources/views/apartments/messages.blade.php
@@ -10,47 +10,48 @@
         </div>
         <div class="container-fluid">
             <div class="row row-cols-1">
-
-
-
-
-
                 @forelse ($messages as $message)
                     <div class="col">
                         <div class="card ms_card d-flex flex-row flex-wrap my-3">
-                            <img width="250" height="150" class="apartment-img"
-                                src="{{ Storage::exists($message->media) ? asset('storage/' . $message->media) : $message->media }}"
-                                alt="Title">
-                            <div class="card-body d-flex justify-content-between align-items-center">
-                                <div class="data ps-5">
+                            <div class="col-3 d-flex  align-items-center">
+                                <img width="250" height="150" class="apartment-img"
+                                    src="{{ Storage::exists($message->media) ? asset('storage/' . $message->media) : $message->media }}"
+                                    alt="Title">
+                                <span class="data data_visible ps-3">
                                     {{ substr($message->created_at, 2, 9) }}
 
-                                </div>
-                                <div class="info d-flex  align-items-center gap-3">
-                                    <div class="img_msg rounded-5 text-white">
+                                </span>
+                            </div>
+                            <div class="col-3 d-flex align-items-center gap-3">
+                                <div class="img_hide">
+                                    <div class="img_msg text-white d-flex justify-content-center align-items-center">
                                         {{ substr($message->name, 0, 1) }}
                                         {{ substr($message->surname, 0, 1) }}
                                     </div>
-                                    <div>
-                                        <h6 class="name">
-                                            {{ $message->name }} {{ $message->surname }}
-                                        </h6>
-                                        <h6>
-                                            {{ $message->email }}
-                                        </h6>
+                                </div>
+                                <div class="info">
+                                    <h6 class="name">
+                                        {{ $message->name }} {{ $message->surname }}
+                                    </h6>
+                                    <h6>
+                                        {{ $message->email }}
+                                    </h6>
+                                    <div class="data data_hide">
+                                        {{ substr($message->created_at, 2, 9) }}
+
                                     </div>
                                 </div>
-                                <div>
-
+                            </div>
+                            <div class="col-6 d-flex justify-content-between align-items-center">
+                                <div class="body ">
                                     <h5>{{ $message->title }}</h5>
-
-
                                     {{ $message->body }}
                                 </div>
                                 <a target=”_blank” href="mailto:{{ $message->email }}" class="btn send_msg_btn me-4">
                                     Rispondi
                                 </a>
                             </div>
+
                         </div>
                     </div>
                 @empty
